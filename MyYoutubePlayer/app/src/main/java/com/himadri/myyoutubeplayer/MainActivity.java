06ccf24037dfb61d9f7ce6b061c7d8ceed221f42
RT <himadri.p@tbitsglobal.com>
2020-01-28 18:28:17 +0530

    My Youtube player app

---
+++ MyYoutubePlayer/app/src/main/java/com/himadri/myyoutubeplayer/MainActivity.java
@@ -0,0 +1,49 @@
+package com.himadri.myyoutubeplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    private static final String TAG = "MainActivity";
+
+    YouTubePlayerView youTubePlayerView;
+    Button playbutton;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        youTubePlayerView = findViewById(R.id.youtubeplayerview);
+
+        playbutton = findViewById(R.id.playbutton);
+        Log.d(TAG, "on starting,..");
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("W4hTJybfU7s");
+                youTubePlayer.pause();
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(YoutubeConfig.getApiKey(), onInitializedListener);
+            }
+        });
+    }
+}
---
+++ MyYoutubePlayer/app/src/main/java/com/himadri/myyoutubeplayer/YoutubeConfig.java
@@ -0,0 +1,10 @@
+package com.himadri.myyoutubeplayer;
+
+public class YoutubeConfig {
+    private final static String API_KEY = "AIzaSyCc1DD-0kS-ens-HK8zj6uT9Y7H836w1M4";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
+
